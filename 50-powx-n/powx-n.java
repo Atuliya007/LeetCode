b145cc8fde8949e35d8ba31dc76a1dfb599a1c76
Atuliya Bansal <atuliyabansal07@gmail.com>
2026-09-08 10:35:50 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (33.76%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,15 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n;
+        long N=n;
         if(N<0){
-            x = 1/x;
-            N = -N;
-        
+            x=1/x;
+            N=-N;
         }
         if(N==0){
             return 1;
         }
-        double half=myPow(x , (int)(N/2));
-        if(N%2==0){
-            return half*half;
-        }
-        else{
-            return x*half*half;
-        }
+        double half=myPow(x,(int)(N/2));
+        if(N%2==0)return half*half;
+        else return x*half*half;
     }
 }
